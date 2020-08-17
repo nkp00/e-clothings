5e645b2f567f56da1a965a06bb6803d30e34fceb
Neeraj Kumar Prajapati <neerajkp501@gmail.com>
2020-08-18 01:07:02 +0530

    Updated with Header component including links to shop and contact

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.scss";
+import Navbar from "./components/navbar/navbar.componet";
 import HomePage from "./pages/homepage";
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
--- src/components/navbar/navbar.componet.jsx
+++ src/components/navbar/navbar.componet.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assests/crown.svg";
+
+import "./navbar.style.scss";
 
 class NavBar extends Component {
   state = {};
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
+      <div className="header">
+        <Link className="logo-container" to="/">
+          <Logo className="logo"></Logo>
+        </Link>
+        <div className="options">
+          <Link to="/shop" className="option">
+            SHOP
+          </Link>
+          <Link to="/contact" className="option">
+            CONTACT
+          </Link>
+        </div>
+      </div>
     );
   }
 }
